3a3c0c2d9861e5970ed78dbb6f625342daab4ca8
Sureya-s <138789786+Sureya-s@users.noreply.github.com>
2025-03-12 13:50:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,8 @@ def summarize():
 # Run the app locally
 import os
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))  # Change to 5001
     app.run(host='0.0.0.0', port=port)
+
 
 
