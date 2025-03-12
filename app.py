ab33f11eb72c6bf8489ab5cef9022e0c0a660442
Sureya-s <138789786+Sureya-s@users.noreply.github.com>
2025-03-12 13:47:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,5 +52,9 @@ def summarize():
     return jsonify({"video_id": video_id, "summary": summary})
 
 # Run the app locally
+import os
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
